5776bb20430c5201023199d878b76dd14f444483
antonova.u <antonova.u@ati.su>
2024-03-27 17:52:33 +0300

    nest: add swagger

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,26 +1,31 @@
 import { Controller, Get, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiOperation({ summary: 'Get all users' })
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
+  @ApiOperation({ summary: 'Get user by id' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
+  @ApiOperation({ summary: 'Update user' })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
+  @ApiOperation({ summary: 'Delete user' })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
